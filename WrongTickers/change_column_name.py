6b8736cea8e16721e6d8b95b963a527250014821
Mark Wang <wangyouan0629@hotmail.com>
2016-07-15 18:17:48 +0800

    Finish file used to change column names

---
+++ WrongTickers/change_column_name.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project: QuestionFromProfWang
+# File name: change_column_name
+# Author: Mark Wang
+# Date: 15/7/2016
+
+import csv
+
+import pandas
+
+name_variable = pandas.read_excel('NewVariableName.xlsx')
+change_variable = name_variable[name_variable['NewVariableName'].notnull()]
+
+with open('SDC_CRSP.csv') as input_file, open('SDC_CRSP2.csv', 'w') as output_file:
+    reader = csv.reader(input_file)
+    writer = csv.writer(output_file)
+    header = next(reader)
+    for i in range(len(header)):
+        info = header[i]
+        for index, row in change_variable.iterrows():
+            if info.startswith(change_variable.ix[index, 'OriginalVariableName']):
+                info_list = info.split('_')
+                while '' in info_list:
+                    info_list.remove('')
+                info_list[0] = change_variable.ix[index, 'NewVariableName']
+                header[i] = '_'.join(info_list)
+                break
+    writer.writerow(header)
+
+    for row in reader:
+        writer.writerow(row)
