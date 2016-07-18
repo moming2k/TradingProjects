74ccbb68c8d4a9b83a9d5acab2cd277acceb338f
Mark Wang <wangyouan0629@hotmail.com>
2016-07-18 13:05:53 +0800

    Change api key to wangyouan7

--- GeoInfoQuery/code/get_church_data_from_google_maps.py
+++ GeoInfoQuery/code/get_church_data_from_google_maps.py
@@ -34,8 +34,11 @@ if __name__ == "__main__":
         # this for prof wang's office computer
         # for i in range(1, 50, process_num):
 
+        # my linux
+        for i in range(201, 210, process_num):
+
         # use for my own one
-        for i in range(200, 210, process_num):
+        # for i in range(102, 110, process_num):
             print "Start test time {}".format(i / process_num)
             lat = south_lat + delta_lat * i
             part_df = get_church_info_along_latitude(latitude=lat)
