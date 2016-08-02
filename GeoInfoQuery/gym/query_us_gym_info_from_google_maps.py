40ceba424a5c0277bf0951ab0c24e0de5b870b3a
Mark Wang <wangyouan0629@hotmail.com>
2016-08-02 19:14:39 +0800

    change gym boundary

--- GeoInfoQuery/gym/query_us_gym_info_from_google_maps.py
+++ GeoInfoQuery/gym/query_us_gym_info_from_google_maps.py
@@ -10,12 +10,12 @@ from GeoInfoQuery.google_maps.query_us_place_information import query_informatio
 
 path = '/'.join(__file__.split('/')[:-1])
 
-boundary = {'west': -121.79826189473684,
-           'east': -120.81416121561969,
-           'north': 49.384358,
-           'south': 24.396308}
+boundary = {'west': -122.73865128613139,
+            'east': -66.885444,
+            'north': 49.384358,
+            'south': 24.396308}
 
 query_information_from_google_maps(query_type='gym', country_code='usa', radius=10000.0, save_path=path,
-                                   # boundary=boundary,
+                                   boundary=boundary,
                                    # previous_file='/Users/warn/PycharmProjects/QuestionFromProfWang/GeoInfoQuery/school/usa_school_info0.csv')
                                    )
