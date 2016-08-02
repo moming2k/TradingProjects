22d7ebf5c4473b23feb6972ddc033f2c3a6d1afb
Mark Wang <wangyouan0629@hotmail.com>
2016-08-02 10:58:12 +0800

    Add a file to fill in missing info

---
+++ GeoInfoQuery/school/fill_in_missing_info.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project: QuestionFromProfWang
+# File name: fill_in_missing_info
+# Author: Mark Wang
+# Date: 2/8/2016
+
+from ..google_maps.query_us_place_information import fill_in_missing_information
+
+file_path = '/Users/warn/PycharmProjects/QuestionFromProfWang/GeoInfoQuery/school/usa_school_info0.csv'
+
+fill_in_missing_information(file_path, keys_to_fill={'url', 'country', 'detail_type'}, start_index=801)
