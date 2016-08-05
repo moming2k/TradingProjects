d30fcca1fd6e549b6291db03427844e7603c3ad3
Mark Wang <wangyouan0629@hotmail.com>
2016-08-05 17:09:16 +0800

    Add book store query file

--- GeoInfoQuery/book_store/query_us_book_store_info_from_google_maps.py
+++ GeoInfoQuery/book_store/query_us_book_store_info_from_google_maps.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Project: QuestionFromProfWang
-# File name: query_us_post_office_info_from_google_maps
+# File name: query_us_book_store_info_from_google_maps
 # Author: Mark Wang
 # Date: 31/7/2016
 
@@ -11,10 +11,10 @@ from ..google_maps.query_us_place_information import query_information_from_goog
 path = '/'.join(__file__.split('/')[:-1])
 
 boundary = {'west': -122.38872230220713,
-           'east': -120.81416121561969,
-           'north': 49.384358,
-           'south': 24.396308}
+            'east': -120.81416121561969,
+            'north': 49.384358,
+            'south': 24.396308}
 
-query_information_from_google_maps(query_type='post_office', country_code='usa', radius=10000.0, save_path=path,
+query_information_from_google_maps(query_type='book_store', country_code='usa', radius=10000.0, save_path=path,
                                    # boundary=boudary
                                    )
