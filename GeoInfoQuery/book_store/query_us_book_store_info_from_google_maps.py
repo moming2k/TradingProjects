b3d6baf4d18ca1988c17670ede4746bb9b4b4c5c
Mark Wang <wangyouan0629@hotmail.com>
2016-08-08 08:50:10 +0800

    change school boundary

--- GeoInfoQuery/book_store/query_us_book_store_info_from_google_maps.py
+++ GeoInfoQuery/book_store/query_us_book_store_info_from_google_maps.py
@@ -10,11 +10,11 @@ from ..google_maps.query_us_place_information import query_information_from_goog
 
 path = '/'.join(__file__.split('/')[:-1])
 
-boundary = {'west': -122.38872230220713,
-            'east': -120.81416121561969,
+boundary = {'west': -91.78725178248175,
+            'east': -66.885444,
             'north': 49.384358,
             'south': 24.396308}
 
 query_information_from_google_maps(query_type='book_store', country_code='usa', radius=10000.0, save_path=path,
-                                   # boundary=boundary
+                                   boundary=boundary
                                    )
