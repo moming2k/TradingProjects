90273e435adb99efffb6f25d1eb3c1008e5128b9
Mark Wang <wangyouan0629@hotmail.com>
2016-08-10 07:22:42 +0800

    change bar boundary

--- GeoInfoQuery/bar/query_us_bar_from_google_maps.py
+++ GeoInfoQuery/bar/query_us_bar_from_google_maps.py
@@ -12,14 +12,14 @@ from ..google_maps.query_us_place_information import query_information_from_goog
 
 path = '/'.join(__file__.split('/')[:-1])
 
-boundary = {'west': -122.38872230220713,
-           'east': -120.81416121561969,
-           'north': 49.384358,
-           'south': 24.396308}
+boundary = {'west': -117.02671119591241,
+            'east': -66.885444,
+            'north': 49.384358,
+            'south': 24.396308}
 
 vdisplay = Xvfb(width=1366, height=768)
 vdisplay.start()
 query_information_from_google_maps(query_type='bar', country_code='usa', radius=6000.0, save_path=path,
-                                   # boundary=boundary
+                                   boundary=boundary
                                    )
 vdisplay.stop()
