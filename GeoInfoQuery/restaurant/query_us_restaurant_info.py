21d933ecee65b11c876c4a3196a2222b647f9714
Mark Wang <wangyouan0629@hotmail.com>
2016-08-14 17:40:53 +0800

    Change boundary information

--- GeoInfoQuery/restaurant/query_us_restaurant_info.py
+++ GeoInfoQuery/restaurant/query_us_restaurant_info.py
@@ -6,16 +6,21 @@
 # Author: Mark Wang
 # Date: 10/8/2016
 
+from xvfbwrapper import Xvfb
+
 from ..google_maps.query_us_place_information import query_information_from_google_maps
 
 path = '/'.join(__file__.split('/')[:-1])
 
-boundary = {'west': -122.38781013834853,
+boundary = {'west': -122.31755397867973,
             'east': -66.885444,
             'north': 49.384358,
             'south': 24.396308}
 
+vdisplay = Xvfb(width=1366, height=768)
+vdisplay.start()
+
 query_information_from_google_maps(query_type='restaurant', country_code='usa', radius=5000.0, save_path=path,
                                    boundary=boundary,
-                                   # previous_file='/Users/warn/PycharmProjects/QuestionFromProfWang/GeoInfoQuery/school/usa_school_info0.csv')
                                    )
+vdisplay.stop()
