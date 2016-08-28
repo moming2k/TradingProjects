591fb4607b732a1a4a9ba8e826d382e012f3f289
Mark Wang <wangyouan0629@hotmail.com>
2016-08-28 22:52:57 +0800

    Finish project about sdc and bloomberg

--- WrongTickers/get_two_years_SDC_daily_data.py
+++ WrongTickers/get_two_years_SDC_daily_data.py
@@ -8,15 +8,16 @@
 
 import datetime
 import os
+import re
+from dateutil.relativedelta import relativedelta
 
 import pandas as pd
 import numpy as np
-# from pandas.tseries.holiday import USFederalHolidayCalendar
-# from pandas.tseries.offsets import CustomBusinessDay
+import pathos
 
 from constants import *
 
-file_path = 'result_csv/wrong_tickers_from_SDC_target_name_sample.csv'
+file_path = 'result_csv/wrong_tickers_from_SDC_target_name(UpdatedIBES).csv'
 df = pd.read_csv(file_path, usecols=[CUSIP_REAL, CUSIP_WRONG, DATE_TODAY, DATE_YESTERDAY,
                                      DATE_TOMORROW, COMPANY_NAME, TICKER_WRONG, TICKER_REAL,
                                      WRONG_TICKER_SOURCE])
@@ -25,8 +26,8 @@ df[DATE_YESTERDAY] = df[DATE_YESTERDAY].apply(lambda x: datetime.datetime.strpti
 df[DATE_TOMORROW] = df[DATE_TOMORROW].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d"))
 ticker_group = df.groupby(TICKER_REAL)
 
-
-# us_cal = CustomBusinessDay(calendar=USFederalHolidayCalendar())
+ibes_df = pd.read_csv('result_csv/IBES_EPS_NUMSET_COUNT.csv', usecols=['CUSIP', 'OFTIC', 'YEAR', 'NUMEST'],
+                      dtype={'CUSIP': str, "YEAR": int, 'NUMEST': int})
 
 
 def get_information_in_CRSP(cusip, ticker, start_date, end_date, info_type):
@@ -103,19 +104,29 @@ def get_information_in_CRSP(cusip, ticker, start_date, end_date, info_type):
         del sub_df['ASKHI']
         del sub_df['BIDLO']
 
-    return sub_df
+    return sub_df[~sub_df.index.duplicated(keep='last')]
 
 
 def get_range_info(cusip, ticker, start_date, end_date):
-    df_list = []
+    result_df = pd.DataFrame()
     for info in [LOG_RETURN, PRICE_RANGE, SIMPLE_RETURN, PRICE, VOLUME, COMPUSTAT]:
-        df_list.append(get_information_in_CRSP(cusip=cusip, ticker=ticker, start_date=start_date, end_date=end_date,
-                                               info_type=info))
+        result_df = result_df.join(get_information_in_CRSP(cusip=cusip, ticker=ticker, start_date=start_date,
+                                                           end_date=end_date, info_type=info),
+                                   how='outer')
 
-    if df_list:
-        return pd.concat(df_list, axis=1)
+    return result_df
+
+
+def get_ibes_info(cusip, ticker, year):
+    sub_df = ibes_df[ibes_df['CUSIP'] == cusip]
+    if sub_df.empty:
+        sub_df = ibes_df[ibes_df['OFTIC'] == ticker]
+
+    sub_df = sub_df[sub_df['YEAR'] == year]
+    if sub_df.empty:
+        return 0
     else:
-        return pd.DataFrame()
+        return sub_df.ix[sub_df.index[0], 'NUMEST']
 
 
 def merge_real_and_wrong_ticker_data(real, wrong):
@@ -144,15 +155,25 @@ def get_two_years_data(ticker_name):
     real_ticker = sub_df.ix[sub_index[0], TICKER_REAL]
     real_cusip = sub_df.ix[sub_index[0], CUSIP_REAL]
     date_today = sub_df.ix[sub_index[0], DATE_TODAY]
+    date_tomorrow = sub_df.ix[sub_index[0], DATE_TOMORROW]
+    date_yesterday = sub_df.ix[sub_index[0], DATE_YESTERDAY]
     source = sub_df.ix[sub_index[0], WRONG_TICKER_SOURCE]
     company_name = sub_df.ix[sub_index[0], COMPANY_NAME]
 
-    start_date = datetime.datetime(year=date_today.year - 1, month=date_today.month, day=date_today.day)
-    end_date = datetime.datetime(year=date_today.year + 1, month=date_today.month, day=date_today.day)
+    start_date = date_today - relativedelta(years=1)
+    end_date = date_today + relativedelta(years=1)
     real_df = get_range_info(real_cusip, real_ticker, start_date, end_date)
     result_list = []
     if real_df.empty:
         return pd.DataFrame()
+
+    real_ibes_date = {date_today.year: get_ibes_info(real_cusip, real_ticker, date_today.year),
+                      date_today.year - 1: get_ibes_info(real_cusip, real_ticker, date_today.year - 1),
+                      date_today.year + 1: get_ibes_info(real_cusip, real_ticker, date_today.year + 1),
+                      }
+
+    if PRICE_HIGH in real_df.keys() and PRICE_LOW in real_df.keys():
+        real_df[PRICE_RANGE] = real_df[PRICE_HIGH] - real_df[PRICE_LOW]
     for index in sub_index:
         wrong_ticker = sub_df.ix[index, TICKER_WRONG]
         wrong_cusip = sub_df.ix[index, CUSIP_WRONG]
@@ -161,6 +182,13 @@ def get_two_years_data(ticker_name):
         if wrong_df.empty:
             continue
 
+        if PRICE_HIGH in wrong_df.keys() and PRICE_LOW in wrong_df.keys():
+            wrong_df[PRICE_RANGE] = wrong_df[PRICE_HIGH] - wrong_df[PRICE_LOW]
+
+        wrong_ibes_date = {date_today.year: get_ibes_info(wrong_cusip, wrong_ticker, date_today.year),
+                           date_today.year - 1: get_ibes_info(wrong_cusip, wrong_ticker, date_today.year - 1),
+                           date_today.year + 1: get_ibes_info(wrong_cusip, wrong_ticker, date_today.year + 1),
+                           }
         temp_df = merge_real_and_wrong_ticker_data(real_df, wrong_df)
         temp_df.loc[:, TICKER_WRONG] = wrong_ticker
         temp_df.loc[:, TICKER_REAL] = real_ticker
@@ -171,11 +199,21 @@ def get_two_years_data(ticker_name):
         temp_df.loc[:, IS_EVENT_DATE] = 0
         temp_df.loc[:, IS_EVENT_TOMORROW] = 0
         temp_df.loc[:, IS_EVENT_YESTERDAY] = 0
-        today_index = (temp_df.index == date_today).nonzero()[0][0]
-        temp_df.ix[today_index, IS_EVENT_DATE] = 1
-        temp_df.ix[today_index - 1, IS_EVENT_YESTERDAY] = 1
-        temp_df.ix[today_index + 1, IS_EVENT_TOMORROW] = 1
+        index = (temp_df.index == date_today).nonzero()[0]
+        if (temp_df.index == date_today).any():
+            temp_df.ix[index[0], IS_EVENT_DATE] = 1
+
+        index = (temp_df.index == date_tomorrow).nonzero()[0]
+        if (temp_df.index == date_tomorrow).any():
+            temp_df.ix[index[0], IS_EVENT_TOMORROW] = 1
+
+        index = (temp_df.index == date_yesterday).nonzero()[0]
+        if (temp_df.index == date_yesterday).any():
+            temp_df.ix[index[0], IS_EVENT_YESTERDAY] = 1
+
         temp_df[DATE] = temp_df.index
+        temp_df["{}_{}".format(IBES_NUM, REAL)] = temp_df[DATE].apply(lambda x: real_ibes_date[x.year])
+        temp_df["{}_{}".format(IBES_NUM, WRONG)] = temp_df[DATE].apply(lambda x: wrong_ibes_date[x.year])
         result_list.append(temp_df)
 
     if result_list:
@@ -190,8 +228,33 @@ def get_two_years_data(ticker_name):
 if __name__ == "__main__":
     ticker_values = ticker_group.groups.keys()
 
-    list_df = []
-    for ticker in ticker_values:
-        list_df.append(get_two_years_data(ticker))
+    process_num = 16
+    pool = pathos.multiprocessing.ProcessingPool(process_num)
 
-    pd.concat(list_df, ignore_index=True, axis=0).to_csv('result_csv/test.csv', encoding='utf8')
+
+    def process_df(values):
+        list_df = []
+        for ticker in values:
+            list_df.append(get_two_years_data(ticker))
+
+        return pd.concat(list_df, ignore_index=True, axis=0)
+
+
+    split_values = np.array_split(ticker_values, process_num)
+    result_dfs = pool.map(process_df, split_values)
+    result_df = pd.concat(result_dfs, ignore_index=True, axis=0)
+    # result_df = process_df(ticker_values)
+    result_df.to_csv('result_csv/wrong_ticker_whole_daily_data_SDC.csv', encoding='utf8')
+    # result_df['TickerNum'] = result_df[WRONG_TICKER_SOURCE].apply(lambda x: re.findall(r'\d+', x)[0])
+    source = result_df[WRONG_TICKER_SOURCE].apply(lambda x: x.split(' ')[-1])
+    sub_df1 = result_df[source == 'initials']
+    sub_df1.to_csv('result_csv/wrong_ticker_initial_daily_data_SDC.csv', encoding='utf8')
+    ticker_num = sub_df1[WRONG_TICKER_SOURCE].apply(lambda x: int(re.findall(r'\d+', x)[0]))
+    sub_df1 = sub_df1[ticker_num > 2]
+    sub_df1.to_csv('result_csv/wrong_ticker_initial_over2_daily_data_SDC.csv', encoding='utf8')
+
+    sub_df1 = result_df[source == 'letters']
+    sub_df1.to_csv('result_csv/wrong_ticker_capital_daily_data_SDC.csv', encoding='utf8')
+
+    sub_df1 = result_df[source == 'name']
+    sub_df1.to_csv('result_csv/wrong_ticker_first_word_daily_data_SDC.csv', encoding='utf8')
