4def1106b320f5f499ce040ef6f7e2168339cee1
Mark Wang <wangyouan0629@hotmail.com>
2016-09-11 22:00:46 +0800

    Finish bootstrap process

--- Bootstrap/python_src/test_utils.py
+++ Bootstrap/python_src/test_utils.py
@@ -6,13 +6,23 @@
 # Author: Mark Wang
 # Date: 11/9/2016
 
+import math
+
 import pandas as pd
 
 
 class SPASRCCalculator(object):
     def __init__(self, data_path, bootstrap_num):
-        self.data_file = pd.read_csv(data_path)
+        self.input_df = pd.read_csv(data_path, index_col=0)
         self.bootstrap_time = bootstrap_num
+        self._mean = self.input_df.mean()
+        self._demean = self.input_df - self._mean
+        self._std = self._demean.std()
+        self._sample_size, self._strategy_num = self.input_df.shape
+        self.sspa_statistics = self._sample_size ** 0.5 * self._mean / self._std
+        self.recenter_vector = self._mean / self._std
+        threshold = - 2 * math.log(math.log(self._sample_size)) ** 0.5
+        self.recenter_vector = self.recenter_vector[self.sspa_statistics <= threshold]
 
     def get_spa(self, diff_type='Sharpe'):
         pass
@@ -29,5 +39,13 @@ class SPASRCCalculator(object):
     def get_stepwise_spa(self, diff_type='Sharpe'):
         pass
 
-    def _bootstrap(self):
-        pass
+    def _bootstrap(self, diff_type):
+        boot_result = pd.DataFrame(columns=self.input_df.keys())
+        for i in range(self.bootstrap_time):
+            if diff_type == 'Sharpe':
+                boot_result.loc[i] = self._demean.sample(self._sample_size,
+                                                         replace=True).mean() / self._std + self.recenter_vector
+            else:
+                boot_result.loc[i] = self._demean.sample(self._sample_size, replace=True).mean()
+
+        return boot_result
