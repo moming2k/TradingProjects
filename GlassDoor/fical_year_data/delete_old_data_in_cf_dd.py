a6da12b824750714171614c61efa3490a51bec57
Mark Wang <wangyouan0629@hotmail.com>
2016-11-05 23:29:42 +0800

    add merge data info

---
+++ GlassDoor/fical_year_data/delete_old_data_in_cf_dd.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project: QuestionFromProfWang
+# File name: delete_old_data_in_cf_dd
+# Author: Mark Wang
+# Date: 5/11/2016
+
+
+import os
+import datetime
+
+import pandas as pd
+
+path = '/home/wangzg/Documents/WangYouan/research/Glassdoor'
+input_path = 'input_data'
+output_path = 'output'
+result_path = 'result'
+data_path = '20161025merge_data'
+
+df = pd.read_csv(os.path.join(path, result_path, 'glassdoor_indicators_addPolMag.csv'),
+                 dtype={'FK_employerId': str, 'FK_dateId': str, 'commentYear': str})
+
+date_index = df['FK_dateId'].apply(lambda x: datetime.datetime.strptime(x, '%Y%m%d'))
+oldest_date = date_index.min()
+
+# handle cf data delete date too old
+cf = pd.read_csv(os.path.join(path, data_path, 'accrual_cf_control.csv'),
+                 dtype={'gvkey': str, 'fyear': str, 'datadate': str, 'gvkey1': str})
+
+cf = cf.dropna(subset=['datadate'])
+
+cf_date = cf['datadate'].apply(lambda x: datetime.datetime.strptime(x, '%Y%m%d'))
+cf = cf[cf_date >= oldest_date]
+cf.to_csv(os.path.join(path, output_path, 'cf_drop_old.csv'), index=False, encoding='utf8')
+
+# handle dd data delete date too old
+dd = pd.read_csv(os.path.join(path, data_path, 'dd_quality_control.csv'),
+                 dtype={'gvkey': str, 'fyear': str, 'datadate': str, 'gvkey1': str})
+dd = dd.dropna(subset=['datadate'])
+dd_date = dd['datadate'].apply(lambda x: datetime.datetime.strptime(x, '%Y%m%d'))
+dd = dd[dd_date >= oldest_date]
+dd.to_csv(os.path.join(path, output_path, 'dd_drop_old.csv'), index=False, encoding='utf8')
+
+# merge dd and cf together
+df = cf.merge(dd, how='outer', on=['fyear', 'tic', 'datadate'], suffixes=['', '_y'])
+
+keys = df.keys()
+for key in keys:
+    if key.endswith('_y') or '{}_y'.format(key) not in keys:
+        continue
+
+    df[key] = df[key].fillna(df['{}_y'.format(key)])
+    del df['{}_y'.format(key)]
+
+df.to_csv(os.path.join(path, output_path, 'cf_dd_merged.csv'), index=False, encoding='utf8')
