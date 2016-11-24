83da82ee99418072a418f8a4db6563657a38a7bb
Mark Wang <wangyouan0629@hotmail.com>
2016-11-25 01:58:44 +0800

    Finish trading strategy

---
+++ StockPrice/TradingStrategy/calculate_strategy_performance.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project: QuestionFromProfWang
+# File name: calculate_strategy_performance
+# Author: Mark Wang
+# Date: 24/11/2016
+
+import numpy as np
+
+
+def calculate_strategy_performance(stock_price, long_position, short_position, start_cash, transaction_cost, delay,
+                                   long_stock=0, short_stock=0):
+    cash_list = [start_cash] * delay
+    stock_long = [long_stock] * delay
+    stock_short = [short_stock] * delay
+    profit = [0] * stock_price.shape[0]
+    start_wealth = start_cash + long_stock + short_stock
+    wealth = [start_wealth] * stock_price.shape[0]
+    pnl = [1] * stock_price.shape[0]
+
+    long_transaction = 0
+    short_transaction = 0
+    for i in range(delay, stock_price.shape[0]):
+        if long_position[i] == 1 and cash_list[-1] > 100:
+            stock_long.append(cash_list[i - 1] * (1 - transaction_cost) + stock_long[-1])
+            stock_short.append(stock_short[-1])
+            cash_list.append(0)
+
+        elif short_position[i] == 1 and cash_list[-1] > 100:
+            stock_short.append(cash_list[-1] * (1 - transaction_cost) + stock_short[-1])
+            stock_long.append(stock_long[-1])
+            cash_list.append(0)
+
+        elif long_position[i] != -1 and stock_long[-1] >0:
+            profit[i] = stock_long[-1] * stock_price[i] / stock_price[i - 1]
+            stock_long.append(stock_long[-1] + profit[i])
+            stock_short.append(stock_short[-1])
+            cash_list.append(cash_list[-1])
+
+        elif short_position != -1 and stock_short[-1] > 0:
+            profit[i] = stock_short[-1] * (1 - stock_price[i] / stock_price[i - 1])
+            stock_short.append(stock_short[-1] + profit[i])
+            stock_long.append(stock_long[-1])
+            cash_list.append(cash_list[-1])
+
+        elif long_position[i] == -1 and stock_long[-1] > 0:
+            profit[i] = stock_long[-1] * (stock_price[i] / stock_price[i - 1] - 1)
+            cash_list.append(stock_long[-1] + profit[i])
+            stock_long.append(0)
+            stock_short.append(stock_short[-1])
+            long_transaction += 1
+
+        elif short_position[i] == -1 and stock_short[-1] > 0:
+            profit[i] = stock_short[-1] * (1 - stock_price[i] / stock_price[i - 1])
+            cash_list.append(stock_short[-1] + profit[i])
+            stock_short.append(0)
+            stock_long.append(stock_long[-1])
+            short_transaction += 1
+
+        else:
+            cash_list.append(cash_list[-1])
+            stock_long.append(stock_long[-1])
+            stock_short.append(stock_short[-1])
+
+        wealth[i] = cash_list[-1] + stock_long[-1] + stock_short[-1]
+        profit[i] = wealth[i] - wealth[0]
+        pnl[i] = wealth[i] / wealth[i - 1] - 1
+
+    sharpe = np.mean(pnl[delay:]) / np.std(pnl[delay:])
+
+    return wealth, sharpe, stock_long, stock_short, cash_list, pnl
---
+++ StockPrice/TradingStrategy/sma_trading_strategy.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project: QuestionFromProfWang
+# File name: sma_trading_strategy
+# Author: Mark Wang
+# Date: 24/11/2016
+
+import os
+import datetime
+
+import pandas as pd
+from talib import abstract
+import numpy as np
+import pathos
+
+from calculate_strategy_performance import calculate_strategy_performance
+from all_stock_list import stock_list
+
+root_path = '/home/wangzg/Documents/WangYouan/research/HongKongStock'
+# root_path = '/Users/warn/PycharmProjects/QuestionFromProfWang/StockPrice'
+data_path = 'YahooStockPrice'
+result_path = '{}SimulateResult'.format(datetime.datetime.today().strftime('%Y%m%d'))
+if not os.path.isdir(os.path.join(root_path, result_path)):
+    os.makedirs(os.path.join(root_path, result_path))
+
+start_cash = 1000000
+delay = 30
+transaction_cost = 0.01
+
+
+def process_df(file_name):
+    stock_price = pd.read_csv(os.path.join(root_path, data_path, file_name), index_col=0)
+    stock_price.dropna(subset=['Volume'], inplace=True)
+    stock_price['Volume'] = stock_price['Volume'].apply(int)
+    stock_price = stock_price[stock_price['Volume'] != 0]
+    rename_dict = {'Volume': 'volume', 'Close': 'close', 'Open': 'open', 'High': 'high', 'Low': 'low'}
+    stock_price.rename(columns=rename_dict, inplace=True)
+
+    stock_price.loc[:, 'date'] = stock_price.index
+    stock_price['date'] = stock_price['date'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
+    stock_price.set_index('date', inplace=True)
+
+    stock_price = stock_price[stock_price.index >= datetime.datetime(2006, 11, 20)]
+    for period1 in range(1, 11):
+        for period2 in range(period1 * 2, 31):
+            for period3 in range(9, 19, 2):
+                # print 'period 1 {} period 2 {} period 3 {}'.format(period1, period2, period3)
+                sma_s = abstract.SMA(stock_price, timeperiod=period1) if period1 != 1 else stock_price['close']
+                sma_l = abstract.SMA(stock_price, timeperiod=period2)
+
+                dma = sma_s - sma_l
+                ama = abstract.SMA(dma.to_frame('close'), timeperiod=period3)
+                diff_ama_dma = dma - ama
+                for threshold in range(0, 61, 6):
+                    threshold = float(threshold) / 10
+                    long_position = [0] * diff_ama_dma.shape[0]
+                    short_position = [0] * diff_ama_dma.shape[0]
+                    for i in range(delay, diff_ama_dma.shape[0]):
+                        if diff_ama_dma[i - 1] < threshold < diff_ama_dma[i]:
+                            long_position[i] = 1
+                            short_position[i - 1] = -1
+
+                        elif diff_ama_dma[i] < -threshold < diff_ama_dma[i - 1]:
+                            long_position[i - 1] = -1
+                            short_position[i] = 1
+
+                    wealth, sharpe, stock_long, stock_short, cash_list, pnl = calculate_strategy_performance(
+                        stock_price=stock_price['close'], start_cash=start_cash,
+                        delay=delay, long_position=long_position,
+                        short_position=short_position, transaction_cost=transaction_cost
+                    )
+
+                    # print sharpe
+
+                    if sharpe > 1:
+                        stock_price['wealth'] = wealth
+                        stock_price['short'] = stock_short
+                        stock_price['long'] = stock_long
+                        stock_price['cash'] = cash_list
+                        stock_price['pnl'] = pnl
+
+                        save_file_name = 'p1_{}_p2_{}_p3_{}_th_{}_sharpe_{:.4f}.csv'.format(period1,
+                                                                                            period2,
+                                                                                            period3,
+                                                                                            threshold,
+                                                                                            sharpe)
+                        if not os.path.isdir(os.path.join(root_path, result_path, file_name[:-4])):
+                            os.makedirs(os.path.join(root_path, result_path, file_name[:-4]))
+                        stock_price.to_csv(os.path.join(root_path, result_path, file_name[:-4], save_file_name))
+
+    return 0
+
+
+def process_list(split):
+    for i in split:
+        process_df(i)
+
+    return 0
+
+
+if __name__ == '__main__':
+    process_num = 15
+    pool = pathos.multiprocessing.ProcessingPool(process_num)
+    split_list = np.array_split(stock_list[:150], process_num)
+    pool.map(process_list, split_list)
