19287add1c10dcdbebea24a4f417e1a8d8735501
Mark Wang <wangyouan0629@hotmail.com>
2016-12-13 13:29:29 +0800

    add new information

---
+++ GlassDoor/src/sort_20161212_loop_result.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Filename: sort_20161212_loop_result
+# @Date: 2016-12-13
+# @Author: Mark Wang
+# @Email: wangyouan@gmial.com
+
+import os
+
+root_path = ''
