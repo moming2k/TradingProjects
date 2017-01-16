b2d710233700d1d7dad27941824b0fcf82da6585
Mark Wang <wangyouan0629@hotmail.com>
2017-01-16 14:19:28 +0800

    add two combination file

---
+++ ChineseStock/combination/calculate_combination_buy_only_senior_brothers.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Filename: calculate_combination_buy_only_senior_brothers
+# @Date: 2017-01-16
+# @Author: Mark Wang
+# @Email: wangyouan@gmial.com
+
+import os
+
+from constant import Constant
+from util_functions import generate_buy_only_return_df, calculate_portfolio_return
+from get_root_path import get_root_path
+
+info_type = 'senior_brothers'
+
+root_path = get_root_path()
+data_path = os.path.join(root_path, 'data')
+temp_path = os.path.join(root_path, 'temp')
+wealth_path = os.path.join(temp_path, 'buy_only_wealth_{}'.format(info_type))
+return_path = os.path.join(temp_path, 'buy_only_return_{}'.format(info_type))
+buy_only_report_data_path = os.path.join(data_path, 'report_info_buy_only')
+
+if not os.path.isdir(buy_only_report_data_path):
+    os.makedirs(buy_only_report_data_path)
+
+if not os.path.isdir(return_path):
+    os.makedirs(return_path)
+
+if not os.path.isdir(wealth_path):
+    os.makedirs(wealth_path)
+
+
+def calculate_return_and_wealth_all(info):
+    const = Constant()
+    portfolio_num = info[const.PORTFOLIO_NUM]
+    holding_days = info[const.HOLDING_DAYS]
+
+    return_df = generate_buy_only_return_df(return_path, holding_days, info_type=info_type)
+
+    wealth_df = calculate_portfolio_return(return_df, portfolio_num)
+    wealth_df.to_pickle(
+        os.path.join(wealth_path, 'buy_only_{}_{}_port_{}d.p'.format(info_type, portfolio_num, holding_days)))
+    wealth_df.to_csv(
+        os.path.join(wealth_path, 'buy_only_{}_{}_port_{}d.csv'.format(info_type, portfolio_num, holding_days)),
+        encoding='utf8')
+
+    return wealth_df
+
+
+if __name__ == '__main__':
+    # calculate_return_and_wealth_all({const.PORTFOLIO_NUM: 5, const.HOLDING_DAYS: 11})
+    # import pathos
+    import multiprocessing
+
+    const = Constant()
+
+    process_num = 30
+
+    portfolio_info = []
+    for portfolio_num in range(5, 101, 5):
+        for holding_days in [3, 5, 10, 22, 33, 44, 55, 66, 77, 88, 99, 110]:
+            portfolio_info.append({const.PORTFOLIO_NUM: portfolio_num, const.HOLDING_DAYS: holding_days})
+
+    # pool = pathos.multiprocessing.ProcessingPool(process_num)
+    pool = multiprocessing.Pool(process_num)
+
+    pool.map(calculate_return_and_wealth_all, portfolio_info)
