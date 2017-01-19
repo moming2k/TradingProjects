0c97b7833d8773a89360ef1c6ac76d4a9a18b292
Mark Wang <wangyouan0629@hotmail.com>
2017-01-19 22:36:46 +0800

    record all data

--- CarryTrade/code/generate_new_table/step3_spa_src_stepwise_test.py
+++ CarryTrade/code/generate_new_table/step3_spa_src_stepwise_test.py
@@ -15,7 +15,7 @@ import numpy as np
 from scipy import stats
 
 from constant import Constant as const
-from path_info import return_data_path
+from path_info import return_data_path, temp_path
 from util_function import date_as_float
 
 
@@ -109,16 +109,20 @@ def generate_table_3or4(test_time=500, bootstrap_num=1000, currency_num='48'):
     for i in [const.MEAN_RETURN, const.SHAPRE_RATIO]:
         for j in [const.ONE_MONTH, const.THREE_MONTH, const.SIX_MONTH, const.TWELVE_MONTH]:
             column_list.append('{} {}'.format(i, j))
-    result_df = pd.DataFrame(columns=column_list, index=[const.STRATEGY_NUM, const.BEST_STRATEGY_DESCRIPTION,
-                                                         const.BEST_STRATEGY_MEAN_RETURN,
-                                                         const.BEST_STRATEGY_SHAPRE_RATIO,
-                                                         const.BEST_STRATEGY_NOMINAL_P_VALUE,
-                                                         const.BEST_STRATEGY_RC_P_VALUE,
-                                                         const.BEST_STRATEGY_ST_P_VALUE,
-                                                         const.AVERAGE_REJECT_NUM,
-                                                         const.MINIMUM_REJECT_NUM,
-                                                         const.MAXIMUM_REJECT_NUM,
-                                                         const.AVERAGE_REJECT_PORTION])
+    index_list = [const.STRATEGY_NUM, const.BEST_STRATEGY_DESCRIPTION,
+                  const.BEST_STRATEGY_MEAN_RETURN,
+                  const.BEST_STRATEGY_SHAPRE_RATIO,
+                  const.BEST_STRATEGY_NOMINAL_P_VALUE,
+                  const.BEST_STRATEGY_RC_P_VALUE,
+                  const.BEST_STRATEGY_ST_P_VALUE, ]
+
+    for i in ['rc', 'pa', 'st']:
+        for j in [const.AVERAGE_REJECT_NUM,
+                  const.MINIMUM_REJECT_NUM,
+                  const.MAXIMUM_REJECT_NUM,
+                  const.AVERAGE_REJECT_PORTION]:
+            index_list.append('{}_{}'.format(j, i))
+    result_df = pd.DataFrame(columns=column_list, index=index_list)
     for file_info in os.listdir(return_data_path):
         if not file_info.endswith('.p') or currency_num not in file_info:
             continue
@@ -157,9 +161,15 @@ def generate_table_3or4(test_time=500, bootstrap_num=1000, currency_num='48'):
 
             best_rc_p_value = 0.0
             best_st_p_value = 0.0
-            total_reject_num = 0.0
-            max_reject_num = float('-inf')
-            min_reject_num = float('inf')
+
+            total_reject_num = {}
+            max_reject_num = {}
+            min_reject_num = {}
+
+            for i in ['rc', 'pa', 'st']:
+                total_reject_num[i] = 0.0
+                max_reject_num[i] = float('-inf')
+                min_reject_num[i] = float('inf')
 
             for i in range(test_time):
                 boot_df = bootstrap(data_df=df, num=bootstrap_num)
@@ -170,6 +180,17 @@ def generate_table_3or4(test_time=500, bootstrap_num=1000, currency_num='48'):
                 st_reject_num = stepwise_spa_test(df, bootstrap_df=boot_df, test_method=key)
 
                 # TODO: understand what reject num should be recorded
+                total_reject_num['rc'] += rc_reject_num
+                min_reject_num['rc'] = min(min_reject_num['rc'], rc_reject_num)
+                max_reject_num['rc'] = max(max_reject_num['rc'], rc_reject_num)
+
+                total_reject_num['pa'] += pa_reject_num
+                min_reject_num['pa'] = min(min_reject_num['pa'], pa_reject_num)
+                max_reject_num['pa'] = max(max_reject_num['pa'], pa_reject_num)
+
+                total_reject_num['st'] += st_reject_num
+                min_reject_num['st'] = min(min_reject_num['st'], st_reject_num)
+                max_reject_num['st'] = max(max_reject_num['st'], st_reject_num)
 
                 best_rc_p_value += rc_p_value
                 best_st_p_value += pa_p_value
@@ -177,9 +198,20 @@ def generate_table_3or4(test_time=500, bootstrap_num=1000, currency_num='48'):
             result_df.loc[const.BEST_STRATEGY_ST_P_VALUE, column_name] = best_st_p_value / test_time
             result_df.loc[const.BEST_STRATEGY_RC_P_VALUE, column_name] = best_rc_p_value / test_time
             result_df.loc[const.BEST_STRATEGY_RC_P_VALUE, column_name] = best_rc_p_value / test_time
-            result_df.loc[const.AVERAGE_REJECT_NUM, column_name] = total_reject_num / test_time
-            result_df.loc[const.MINIMUM_REJECT_NUM, column_name] = min_reject_num
-            result_df.loc[const.MAXIMUM_REJECT_NUM, column_name] = max_reject_num
-            result_df.loc[const.AVERAGE_REJECT_PORTION, column_name] = total_reject_num / float(test_time) / df.shape[1]
+
+            for i in ['rc', 'pa', 'st']:
+                result_df.loc['{}_{}'.format(const.AVERAGE_REJECT_NUM, i), column_name] = float(
+                    total_reject_num[i]) / test_time
+                result_df.loc['{}_{}'.format(const.MAXIMUM_REJECT_NUM, i), column_name] = max_reject_num[i]
+                result_df.loc['{}_{}'.format(const.MINIMUM_REJECT_NUM, i), column_name] = min_reject_num[i]
+                result_df.loc['{}_{}'.format(const.AVERAGE_REJECT_PORTION, i), column_name] = float(
+                    total_reject_num[i]) / test_time / df.shape[1]
 
     return result_df
+
+
+if __name__ == '__main__':
+    result_df = generate_table_3or4(currency_num='48')
+    result_df.to_pickle(os.path.join(temp_path, 'table3.p'))
+    result_df = generate_table_3or4(currency_num='15')
+    result_df.to_pickle(os.path.join(temp_path, 'table4.p'))
